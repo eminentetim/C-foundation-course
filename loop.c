c7d66afc3401694d59be0c87b82e270b1a6b3095
eminentetim <etimeminent@gmail.com>
2023-05-27 15:50:47 -0400

    modification

---
+++ loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+	double a, b, c, sum;
+	
+	printf("Enter a:");
+	scanf("%f");
+	 printf("Enter b:");
+         scanf("%f");
+	 printf("Enter c:");
+         scanf("%f");
+ 	
+	sum = a + b + c;
+	printf("The sum is %lf\n");
+}
